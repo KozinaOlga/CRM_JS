22345158c29b229749021c66f3039a80f36eb77a
KozinaOlga <kozinaov.27@gmail.com>
2024-01-15 16:09:34 +1000

    исправление ошибок

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 'use strict';
-const nameOfProdact = 'table';
-const quantityOfProdact = 10;
-const prodactCategory = 'furniture';
-const priceOfProdact = 100000;
+const nameOfProdact = 'Утюг Atlanta ATH-5537 фиолетовый';
+const quantityOfProdact = 20;
+const prodactCategory = 'Техника для дома';
+const priceOfProdact = 750;
 
 console.log(nameOfProdact);
 
+const total = quantityOfProdact * priceOfProdact;
+console.log(total);
 
