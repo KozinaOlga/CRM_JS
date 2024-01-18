39ea760c85cd33c5de6265a8149d12df3b21bd18
KozinaOlga <91415534+KozinaOlga@users.noreply.github.com>
2024-01-18 16:35:29 +1000

    Add files via upload

--- new/index.js
+++ new/index.js
@@ -1,1 +1,12 @@
-    
+'use strict';
+const nameOfProdact = 'Утюг Atlanta ATH-5537 фиолетовый';
+const quantityOfProdact = 20;
+const prodactCategory = 'Техника для дома';
+const priceOfProdact = 750;
+
+console.log(nameOfProdact);
+
+const total = quantityOfProdact * priceOfProdact;
+console.log(total);
+
+
