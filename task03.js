9fe2bc730276ffddd2ba5ba08142a78ed6c65665
KozinaOlga <kozinaov.27@gmail.com>
2024-02-05 15:45:31 +1000

    задачи js

--- task03.js
+++ task03.js
@@ -2,37 +2,43 @@
 
 //const amount = 30000; //сумму превышающия 30000
 
-let goods = 0; //кол-во товара в корзине
-let discount = 0; // скидка
+//let goods = 0; //кол-во товара в корзине
+let discount = 0;
+const countThreePercent = 0.03; // скидка 3%
+const countFifteenPercent = 0.15; // скидка 15%
+const countTenPercent = 0.1; // скидка 10%
 
 
-
-const calculate = (totalGoods, quantityItems, promotionalCode = null) => {
+const calculate = (totalGoods, quantityItems, promotionalCode) => {
     let amoutGoods = totalGoods;
     if (quantityItems > 10) {
-        const countThreePcocent = amoutGoods * 0.03; //скидка на товар 3%
-        amoutGoods -= countThreePcocent; //стоимость товара - скидка
+        discount = amoutGoods * countThreePercent; //скидка на товар 3%
+        console.log(discount);
+        amoutGoods -= discount;
+        console.log(amoutGoods);
     }
 
     if (amoutGoods > 30000) {
-        discount = (amoutGoods - 30000) * 0.15;
-        console.log(discount);
+        discount = (amoutGoods - 30000) * countFifteenPercent;
+        amoutGoods -= discount;
+        console.log(amoutGoods);
     }
 
     if (promotionalCode === 'MEHTED') {
-        discount = amoutGoods * 0.1;
-        console.log(discount);
+        discount = amoutGoods * countTenPercent;
+        amoutGoods -= discount;
+        console.log(amoutGoods);
     }
 
     if (promotionalCode === 'G3H2Z1' && (amoutGoods - discount) > 2000) {
         discount = amoutGoods - 500;
-        console.log(discount);
+        console.log(amoutGoods);
     }
 
     return totalGoods;
 }
 
-const res = calculate(50000, 5, 'G3H2Z1');
+const res = calculate(20000, 16, 'G3H2Z1');
 
 console.log(res);
 
